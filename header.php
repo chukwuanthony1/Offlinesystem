8047aa6ad449368db2051f3f32ba54c0a3a1cc8c
chukwuanthony1 <49498863+chukwuanthony1@users.noreply.github.com>
2020-09-07 01:33:02 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
+    <!--<meta http-equiv="refresh" content="900;url=logout.php" /> -->
+    <title>Offline System</title>
+  </head>
+  <body>
+	<?php include_once "inc/navbar.php";?>
